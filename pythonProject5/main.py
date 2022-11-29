9c3a14af70c5828bab29ac18b868c78eff13767c
Guru Sivaram <guru@Gurus-MacBook-Pro.local>
2022-11-29 18:13:27 +0530

    Hangman task complete

--- pythonProject5/main.py
+++ pythonProject5/main.py
@@ -93,10 +93,20 @@ nr_letters= int(input("How many letters would you like in your password?\n"))
 nr_symbols = int(input(f"How many symbols would you like?\n"))
 nr_numbers = int(input(f"How many numbers would you like?\n"))
 
+#Your code here
 #Eazy Level - Order not randomised:
 #e.g. 4 letter, 2 symbol, 2 number = JduE&!91
 
 unOrderedList = []
+test = ""
+
+test1 = "hi"
+test2 = "bye"
+
+test3 = test1 + test2
+print(test3)
+
+
 for letter in range(0, nr_letters):
     print(random.choice(letters), end="")
 for symbol in range(0, nr_symbols):
---
+++ pythonProject6/main.py
@@ -0,0 +1,31 @@
+def my_function():
+    print("Hello World")
+
+my_function()
+
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+
+def jump():
+    turn_left()
+    while wall_on_right():
+        move()
+    turn_right()
+    move()
+    turn_right()
+    while front_is_clear():
+        move()
+    turn_left()
+
+
+while not at_goal():
+    if front_is_clear():
+        move()
+    elif front_is_clear() and wall_on_right():
+        move()
+    elif wall_in_front():
+        jump()
